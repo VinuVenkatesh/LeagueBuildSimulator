125ff2d0e48080d265321b5a19c2351b2c99ad2c
Vinu Venkatesh <kvsvinuvenkatesh@gmail.com>
2024-08-25 15:20:45 -0400

    Added buttons and navigation to champion and items

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/components/home/home.component';
+import { ChampionsComponent } from './features/champions/component/champions/champions.component';
+import { ItemsComponent } from './features/items/component/items/items.component';
 
 export const routes: Routes = [
-    {path:'',component: HomeComponent}
+    {path:'',component: HomeComponent},
+    {path:'champions',component:ChampionsComponent},
+    {path:'items', component:ItemsComponent}
 ];
